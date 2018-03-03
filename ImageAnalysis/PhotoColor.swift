52669fbe64026c71b0af315bfb79b687622755b0
李超逸 <icylydia@gmail.com>
2018-03-04 00:06:24 +0900

    upload photo

---
+++ ImageAnalysis/PhotoColor.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoColor.swift
+//  ImageAnalysis
+//
+//  Created by 李超逸 on 2018/03/03.
+//  Copyright © 2018 李超逸. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoColor {
+    var red: Int?
+    var green: Int?
+    var blue: Int?
+    var colorName: String?
+}
--- ImageAnalysis/ViewController.swift
+++ ImageAnalysis/ViewController.swift
@@ -10,18 +10,25 @@ import Alamofire
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var choosePhotoButton: UIButton!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var tags: [String]?
+    var colors: [PhotoColor]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func choosePhotoButonPressed(_ sender: Any) {
         let picker = UIImagePickerController()
         picker.delegate = self
@@ -49,9 +56,82 @@ extension ViewController: UIImagePickerControllerDelegate {
         
         imageView.image = image
         
+        choosePhotoButton.isHidden = true
+        progressView.progress = 0.0
+        progressView.isHidden = false
+        activityIndicatorView.startAnimating()
+        
+        upload(
+            image: image,
+            progressCompletion: { [unowned self] percent in
+                self.progressView.setProgress(percent, animated: true)
+            },
+            completion: { [unowned self] tags, colors in
+                self.choosePhotoButton.isHidden = false
+                self.progressView.isHidden = true
+                self.activityIndicatorView.stopAnimating()
+                
+                self.tags = tags
+                self.colors = colors
+                
+//                self.performSegue(withIdentifier: "ShowResults", sender: self)
+        })
+        
         dismiss(animated: true)
     }
 }
 
 extension ViewController: UINavigationControllerDelegate {
+}
+
+extension ViewController {
+    func upload(image: UIImage,
+                progressCompletion: @escaping (_ percent: Float) -> Void,
+                completion: @escaping (_ tags: [String], _ colors: [PhotoColor]) -> Void) {
+        guard let imageData = UIImageJPEGRepresentation(image, 0.5) else {
+            print("Could not get JPEG representation of UIImage")
+            return
+        }
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(imageData,
+                                         withName: "imagefile",
+                                         fileName: "image.jpg",
+                                         mimeType: "image/jpeg")
+        },
+            to: "http://api.imagga.com/v1/content",
+            headers: ["Authorization": "Basic YWNjXzcxNTEwZTczNTc1YmVmZjpkOTMwZjA4ZDRlNWVlMWI5MTg5NzEyNjlmNzc5OTBlYQ=="],
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .success(let upload, _, _):
+                    upload.uploadProgress { progress in
+                        progressCompletion(Float(progress.fractionCompleted))
+                    }
+                    upload.validate()
+                    upload.responseJSON { response in
+                        guard response.result.isSuccess else {
+                            print("Error while uploading file: \(response.result.error)")
+                            completion([String](), [PhotoColor]())
+                            return
+                        }
+                        
+                        guard let responseJSON = response.result.value as? [String: Any],
+                            let uploadedFiles = responseJSON["uploaded"] as? [[String: Any]],
+                            let firstFile = uploadedFiles.first,
+                            let firstFileID = firstFile["id"] as? String else {
+                                print("Invalid information received from service")
+                                completion([String](), [PhotoColor]())
+                                return
+                        }
+                        
+                        print("Content uploaded with ID: \(firstFileID)")
+                        
+                        completion([String](), [PhotoColor]())
+                    }
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
+        }
+        )
+    }
 }
